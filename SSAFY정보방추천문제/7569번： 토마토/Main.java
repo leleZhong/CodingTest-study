6daa9361663ad98dadbd1e3893b96e217f705076
Zhong <20201783@o365.hanbat.ac.kr>
2025-03-14 23:07:05 +0900

    BOJ-EX: 2025. 3. 14. 오후 11:07:04

--- SSAFY정보방추천문제/7569번： 토마토/Main.java
+++ SSAFY정보방추천문제/7569번： 토마토/Main.java
@@ -12,6 +12,8 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -27,58 +29,49 @@ public class Main {
         M = Integer.parseInt(st.nextToken());
         N = Integer.parseInt(st.nextToken());
         H = Integer.parseInt(st.nextToken());
-        tomatoes = new int[H][M][N];
+        tomatoes = new int[H][N][M];
+        Queue<int[]> queue = new LinkedList<>();
+
+        int unripeCnt = 0;
         for (int i = 0; i < H; i++) {
-            for (int j = 0; j < M; j++) {
-                for (int k = 0; k < N; k++) {
-                    st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                st = new StringTokenizer(br.readLine());
+                for (int k = 0; k < M; k++) {
                     tomatoes[i][j][k] = Integer.parseInt(st.nextToken());
+                    if (tomatoes[i][j][k] == 1)
+                        queue.offer(new int[]{i, j, k, 0});
+                    else if (tomatoes[i][j][k] == 0)
+                        unripeCnt++;
                 }
             }
+        }
+
+        if (unripeCnt == 0) {
+            System.out.print(0);
+            return;
         }
 
         int days = 0;
-        while (true) {
-            boolean isAnyTomatoUnripe = false;
-            for (int i = 0; i < H; i++) {
-                for (int j = 0; j < M; j++) {
-                    for (int k = 0; k < N; k++) {
-                        if (tomatoes[i][j][k] == 0) {
-                            if (!checkNearbyTomatoes(i, j, k)) {
-                                System.out.print(-1);
-                                return;
-                            }
-                            isAnyTomatoUnripe = true;
-                        }
-                    }
+        while (!queue.isEmpty()) {
+            int[] curr = queue.poll();
+            int x = curr[0], y = curr[1], z = curr[2], day = curr[3];
+            for (int i = 0; i < 6; i++) {
+                int nx = x + layers[i];
+                int ny = y + rows[i];
+                int nz = z + cols[i];
+
+                if (isValid(nx, ny, nz) && tomatoes[nx][ny][nz] == 0) {
+                    tomatoes[nx][ny][nz] = 1;
+                    queue.offer(new int[]{nx, ny, nz, day + 1});
+                    days = Math.max(days, day + 1);
+                    unripeCnt--;
                 }
             }
-            if (isAnyTomatoUnripe)
-                days++;
-            else {
-                System.out.print(days);
-                return;
-            }
         }
-    }
-
-    static boolean checkNearbyTomatoes(int x, int y, int z) {
-        boolean isTomatoNearby = false;
-        for (int i = 0; i < 6; i++) {
-            int currX = x + layers[i];
-            int currY = y + rows[i];
-            int currZ = z + cols[i];
-            if (!isValid(currX, currY, currZ) || tomatoes[currX][currY][currZ] != 1)
-                continue;
-            if (tomatoes[currX][currY][currZ] != -1)
-                isTomatoNearby = true;
-            else if (tomatoes[currX][currY][currZ] == 1)
-                tomatoes[x][y][z] = 1;
-        }
-        return isTomatoNearby;
+        System.out.print(unripeCnt == 0 ? days : -1);
     }
 
     static boolean isValid(int x, int y, int z) {
-        return x >= 0 && x < H && y >= 0 && y < M && z >= 0 && z < N;
+        return x >= 0 && x < H && y >= 0 && y < N && z >= 0 && z < M;
     }
 }
