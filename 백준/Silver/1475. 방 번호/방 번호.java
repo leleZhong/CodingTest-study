03bd38db66b88ef76a780e9c1fe3918fbe94e77f
Zhong <105035178+leleZhong@users.noreply.github.com>
2025-04-13 23:02:48 +0900

    [Silver V] Title: 방 번호, Time: 64 ms, Memory: 11504 KB -BaekjoonHub

--- 백준/Silver/1475. 방 번호/방 번호.java
+++ 백준/Silver/1475. 방 번호/방 번호.java
@@ -1,27 +1,25 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         char[] N = br.readLine().toCharArray();
 
-        int[] isUsed = new int[10];
-        int sixCnt = 0;
+        int[] nums = new int[10];
         for (char c : N) {
             int num = c - '0';
-
-            if (num == 6 || num == 9) {
-                sixCnt++;
-                continue;
-            }
-            isUsed[num]++;
+            nums[num]++;
         }
-        Arrays.sort(isUsed);
+        nums[6] = nums[9] = (nums[6] + nums[9] + 1) / 2;
+        
+        int result = 0;
+        for (int i : nums) {
+            if (result < i)
+                result = i;
+        }
 
-        sixCnt = (sixCnt % 2 == 0) ? sixCnt / 2 : sixCnt / 2 + 1;
-        System.out.print(Math.max(isUsed[9], sixCnt));
+        System.out.print(result);
     }
 }
