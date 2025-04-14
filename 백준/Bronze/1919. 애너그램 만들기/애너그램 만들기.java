b4781288ef8cef24c2b4b23a08144b252dfd2bca
Zhong <105035178+leleZhong@users.noreply.github.com>
2025-04-14 14:16:08 +0900

    [Bronze II] Title: 애너그램 만들기, Time: 68 ms, Memory: 11488 KB -BaekjoonHub

--- 백준/Bronze/1919. 애너그램 만들기/애너그램 만들기.java
+++ 백준/Bronze/1919. 애너그램 만들기/애너그램 만들기.java
@@ -5,18 +5,18 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        char[] a = br.readLine().toCharArray();
-        char[] b = br.readLine().toCharArray();
+        String a = br.readLine();
+        String b = br.readLine();
 
         int[] aCnt = new int[26];
         int[] bCnt = new int[26];
 
-        for (int i = 0; i < a.length; i++) {
-            aCnt[a[i] - 'a']++;
+        for (int i = 0; i < a.length(); i++) {
+            aCnt[a.charAt(i) - 'a']++;
         }
 
-        for (int i = 0; i < b.length; i++) {
-            bCnt[b[i] - 'a']++;
+        for (int i = 0; i < b.length(); i++) {
+            bCnt[b.charAt(i) - 'a']++;
         }
 
         int result = 0;
