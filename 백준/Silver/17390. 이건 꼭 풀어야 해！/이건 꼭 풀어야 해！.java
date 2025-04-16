bf3fae799bc658844b244719fba4df910b646938
Zhong <105035178+leleZhong@users.noreply.github.com>
2025-04-16 16:37:23 +0900

    [Silver III] Title: 이건 꼭 풀어야 해!, Time: 1384 ms, Memory: 149720 KB -BaekjoonHub

---
+++ 백준/Silver/17390. 이건 꼭 풀어야 해！/이건 꼭 풀어야 해！.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int Q = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+
+        int[] sumArr = new int[N + 1];
+        for (int i = 1; i <= N; i++) {
+            sumArr[i] = sumArr[i - 1] + arr[i - 1];
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < Q; i++) {
+            st = new StringTokenizer(br.readLine());
+            int L = Integer.parseInt(st.nextToken());
+            int R = Integer.parseInt(st.nextToken());
+            sb.append(sumArr[R] - sumArr[L - 1]).append("\n");
+        }
+
+        System.out.print(sb);
+    }
+}
