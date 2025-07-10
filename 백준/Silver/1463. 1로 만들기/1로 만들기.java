7d04455442aa4af8dd526e8a54b08cfeabca6406
Zhong <105035178+leleZhong@users.noreply.github.com>
2025-07-10 15:00:19 +0900

    [Silver III] Title: 1로 만들기, Time: 64 ms, Memory: 12504 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,21 +1,43 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Queue;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        int[] dp = new int[N + 1];
-        dp[1] = 0;
+        boolean[] visited = new boolean[N + 1];
+        Queue<Integer> q = new ArrayDeque<>();
+        q.add(N);
+        visited[N] = true;
+        int depth = 0; // 연산 횟수
 
-        for (int i = 2; i <= N; i++) {
-            dp[i] = dp[i - 1] + 1;
-            if (i % 2 == 0)
-                dp[i] = Math.min(dp[i], dp[i / 2] + 1);
-            if (i % 3 == 0)
-                dp[i] = Math.min(dp[i], dp[i / 3] + 1);
+        while (!q.isEmpty()) {
+            int size = q.size();
+            for (int i = 0; i < size; i++) {
+                int cur = q.poll();
+                if (cur == 1) {
+                    System.out.print(depth);
+                    return;
+                }
+
+                if (cur % 3 == 0 && !visited[cur / 3]) {
+                    visited[cur / 3] = true;
+                    q.add(cur / 3);
+                }
+                if (cur % 2 == 0 && !visited[cur / 2]) {
+                    visited[cur / 2] = true;
+                    q.add(cur / 2);
+                }
+                if (!visited[cur - 1]) {
+                    visited[cur - 1] = true;
+                    q.add(cur - 1);
+                }
+            }
+            depth++;
         }
-        System.out.print(dp[N]);
+        System.out.print(depth);
     }
 }
