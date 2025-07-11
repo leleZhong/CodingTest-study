291429b1dac883ab82e4f454e90ff6e688c9bd12
Zhong <105035178+leleZhong@users.noreply.github.com>
2025-07-11 17:30:48 +0900

    [Silver III] Title: 임스의 데일리 인증 스터디, Time: 236 ms, Memory: 20020 KB -BaekjoonHub

--- 백준/Silver/29730. 임스의 데일리 인증 스터디/임스의 데일리 인증 스터디.java
+++ 백준/Silver/29730. 임스의 데일리 인증 스터디/임스의 데일리 인증 스터디.java
@@ -1,36 +1,40 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        String[] arr = new String[N];
+        ArrayList<String> baekjoon = new ArrayList<>();
+        ArrayList<String> etc = new ArrayList<>();
         for (int i = 0; i < N; i++) {
-            arr[i] = br.readLine();
+            String s = br.readLine();
+            if (s.length() > 7 && s.substring(0, 7).equals("boj.kr/")) {
+                baekjoon.add(s);
+            } else {
+                etc.add(s);
+            }
         }
 
-        Arrays.sort(arr, (a, b) -> {
-            boolean aBoj = a.length() > 7 && a.substring(0, 7).equals("boj.kr/");
-            boolean bBoj = b.length() > 7 && b.substring(0, 7).equals("boj.kr/");
-            if (aBoj && bBoj) {
-                return Integer.parseInt(a.substring(7)) - Integer.parseInt(b.substring(7));
-            } else if (aBoj) {
-                return 1;
-            } else if (bBoj) {
-                return -1;
-            } else {
-                if (a.length() != b.length()) {
-                    return a.length() - b.length();
-                }
-                return a.compareTo(b);
+        Collections.sort(baekjoon, (a, b) -> {
+            return Integer.parseInt(a.substring(7)) - Integer.parseInt(b.substring(7));
+        });
+        
+        Collections.sort(etc, (a, b) -> {
+            if (a.length() != b.length()) {
+                return a.length() - b.length();
             }
+            return a.compareTo(b);
         });
 
         StringBuilder sb = new StringBuilder();
-        for (String s : arr) {
+        for (String s : etc) {
+            sb.append(s + "\n");
+        }
+        for (String s : baekjoon) {
             sb.append(s + "\n");
         }
 
