957af53818278d7f0692c6f84b6205da3b953989
Zhong <105035178+leleZhong@users.noreply.github.com>
2025-07-12 20:06:19 +0900

    [Bronze III] Title: 방어율 무시 계산하기, Time: 228 ms, Memory: 23248 KB -BaekjoonHub

--- 백준/Bronze/25756. 방어율 무시 계산하기/방어율 무시 계산하기.java
+++ 백준/Bronze/25756. 방어율 무시 계산하기/방어율 무시 계산하기.java
@@ -12,8 +12,9 @@ public class Main {
         StringBuilder sb = new StringBuilder();
         double V = 0;
         while (N --> 0) {
-            V = (1 - (1 - V / 100) * (1 - Double.parseDouble(st.nextToken()) / 100)) * 100;
-            sb.append(String.format("%.6f", V)).append("\n");
+            int A = Integer.parseInt(st.nextToken());
+            V = A + V - V * A / 100;
+            sb.append(V).append("\n");
         }
 
         System.out.print(sb);
