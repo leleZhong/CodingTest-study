a6f9bc6821067c5c7fb8058afc087e96c94720a3
Zhong <105035178+leleZhong@users.noreply.github.com>
2025-07-13 11:39:16 +0900

    [Bronze III] Title: Gift Expire Date, Time: 64 ms, Memory: 11548 KB -BaekjoonHub

--- 백준/Bronze/28454. Gift Expire Date/Gift Expire Date.java
+++ 백준/Bronze/28454. Gift Expire Date/Gift Expire Date.java
@@ -1,35 +1,18 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), "-");
-        int curY = Integer.parseInt(st.nextToken());
-        int curM = Integer.parseInt(st.nextToken());
-        int curD = Integer.parseInt(st.nextToken());
-
+        String cur = br.readLine();
         int N = Integer.parseInt(br.readLine());
+
         int cnt = 0;
         while (N-- > 0) {
-            st = new StringTokenizer(br.readLine(), "-");
-            int Y = Integer.parseInt(st.nextToken());
-            int M = Integer.parseInt(st.nextToken());
-            int D = Integer.parseInt(st.nextToken());
-            if (Y > curY) {
+            String s = br.readLine();
+            if (s.compareTo(cur) >= 0) {
                 cnt++;
-                continue;
-            } else if (Y == curY) {
-                if (M > curM) {
-                    cnt++;
-                    continue;
-                } else if (M == curM) {
-                    if (D >= curD) {
-                        cnt++;
-                    }
-                }
             }
         }
 
