f9d1c056b3ff7c569c4f926ec73cd3489e3f5ffe
Zhong <105035178+leleZhong@users.noreply.github.com>
2025-07-16 17:00:06 +0900

    [Bronze V] Title: 대소문자 바꾸기, Time: 72 ms, Memory: 11448 KB -BaekjoonHub

--- 백준/Bronze/2744. 대소문자 바꾸기/대소문자 바꾸기.java
+++ 백준/Bronze/2744. 대소문자 바꾸기/대소문자 바꾸기.java
@@ -5,10 +5,9 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String s = br.readLine();
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < s.length(); i++) {
-            sb.append(s.charAt(i) < 'a' ? (char)(s.charAt(i) + 'a' - 'A') : (char)(s.charAt(i) + 'A' - 'a'));
+        for (char c : br.readLine().toCharArray()) {
+            sb.append(c < 'a' ? (char)(c + 'a' - 'A') : (char)(c + 'A' - 'a'));
         }
 
         System.out.print(sb);
