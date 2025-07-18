0ec0c55ea7beea916977b6269f9a13f5f8aeb475
Zhong <105035178+leleZhong@users.noreply.github.com>
2025-07-18 12:16:38 +0900

    [Bronze III] Title: ACM 호텔, Time: 72 ms, Memory: 12184 KB -BaekjoonHub

---
+++ 백준/Bronze/10250. ACM 호텔/ACM 호텔.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+        while (T --> 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int H = Integer.parseInt(st.nextToken());
+            st.nextToken();
+            int N = Integer.parseInt(st.nextToken());
+            sb.append(N % H != 0 ? N % H : H).append(String.format("%02d", N % H != 0 ? N / H + 1 : N / H)).append("\n");
+        }
+
+        System.out.print(sb);
+    }
+}
