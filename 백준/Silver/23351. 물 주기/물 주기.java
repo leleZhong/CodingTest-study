fd14854b9c2db6fb1ee91675a6907f05b6f3d5cb
Zhong <105035178+leleZhong@users.noreply.github.com>
2025-07-20 00:01:42 +0900

    [Silver III] Title: 물 주기, Time: 68 ms, Memory: 11532 KB -BaekjoonHub

---
+++ 백준/Silver/23351. 물 주기/물 주기.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[N];
+        Arrays.fill(arr, K);
+
+        int idx = 0;
+        int ans = 1;
+        while (true) {
+            for (int i = 0; i < A; i++) {
+                arr[idx + i] += B;
+            }
+            idx += A;
+            idx %= N;
+
+            for (int i = 0; i < N; i++) {
+                if (--arr[i] == 0) {
+                    System.out.print(ans);
+                    return;
+                }
+            }
+
+            ans++;
+        }
+    }
+}
