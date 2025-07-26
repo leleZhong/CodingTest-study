4a6ac9a28f051c3267eaf69ab456f3d121f02d8b
Zhong <105035178+leleZhong@users.noreply.github.com>
2025-07-26 22:56:44 +0900

    [Silver II] Title: 스택 수열, Time: 236 ms, Memory: 26184 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.java
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -1,37 +1,36 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Stack;
+import java.util.ArrayDeque;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        int[] nums = new int[n];
+
+        int[] arr = new int[n];
         for (int i = 0; i < n; i++) {
-            nums[i] = Integer.parseInt(br.readLine());
+            arr[i] = Integer.parseInt(br.readLine());
         }
 
         StringBuilder sb = new StringBuilder();
-        Stack<Integer> stack = new Stack<>();
-        
-        int curr = 1;
-        stack.push(curr);
-        for (int i = 0; i < n; i++) {
-            while (curr <= nums[i]) {
-                stack.push(curr++);
-                sb.append('+').append("\n");
-            }
-            if (stack.peek() == nums[i]) {
+        ArrayDeque<Integer> stack = new ArrayDeque<>();
+        int idx = 0;
+        int num = 1;
+        while (idx < n) {
+            if (stack.isEmpty() || stack.peek() < arr[idx]) {
+                stack.push(num);
+                sb.append("+\n");
+                num++;
+            } else if (stack.peek() == arr[idx]) {
                 stack.pop();
-                sb.append('-').append("\n");
-            }
-            else {
+                sb.append("-\n");
+                idx++;
+            } else {
                 System.out.print("NO");
                 return;
             }
         }
-
         System.out.print(sb);
     }
 }
