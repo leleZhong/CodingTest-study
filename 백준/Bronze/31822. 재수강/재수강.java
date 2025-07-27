05e180c76a297c44e12899b478d666fabeef5925
Zhong <105035178+leleZhong@users.noreply.github.com>
2025-07-27 23:43:28 +0900

    [Bronze IV] Title: 재수강, Time: 68 ms, Memory: 11460 KB -BaekjoonHub

---
+++ 백준/Bronze/31822. 재수강/재수강.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String code = br.readLine().substring(0, 5);
+        int N = Integer.parseInt(br.readLine());
+
+        int cnt = 0;
+        while (N --> 0) {
+            if (br.readLine().substring(0, 5).equals(code)) {
+                cnt++;
+            }
+        }
+
+        System.out.print(cnt);
+    }
+}
