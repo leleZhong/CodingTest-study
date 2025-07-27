91e8c11b107194417bb3d3e709ee6cb045235d1b
Zhong <105035178+leleZhong@users.noreply.github.com>
2025-07-27 20:36:41 +0900

    [Bronze II] Title: Hashing, Time: 64 ms, Memory: 11452 KB, Score: 50 point -BaekjoonHub

---
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int L = Integer.parseInt(br.readLine());
+        char[] arr = br.readLine().toCharArray();
+
+        int r = 31;
+        int M = 1234567891;
+        long hash = 0;
+        for (int i = 0; i < L; i++) {
+            long tmp = 1;
+            for (int j = 0; j < i; j++) {
+                tmp *= 31 % M;
+            }
+            hash += (arr[i] - 'a' + 1) * tmp % M;
+        }
+
+        System.out.print(hash);
+    }
+}
