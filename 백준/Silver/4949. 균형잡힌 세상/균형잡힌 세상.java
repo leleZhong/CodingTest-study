3d91c98c7287ad335e9d8484c5987013f0bbf386
Zhong <105035178+leleZhong@users.noreply.github.com>
2025-07-28 21:21:23 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 124 ms, Memory: 17096 KB -BaekjoonHub

--- 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.java
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.java
@@ -1,45 +1,41 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Stack;
+import java.util.ArrayDeque;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
         StringBuilder sb = new StringBuilder();
-        char[] open = {'(', '['};
-        char[] close = {')', ']'};
-
-        while (true) {
-            String input = br.readLine();
-            if (input.equals("."))
-                break;
-            
+        String input;
+        while (!(input = br.readLine()).equals(".")) {
             char[] arr = input.toCharArray();
-            Stack<Character> bracket = new Stack<>();
-            boolean isBalanced = true;
+            ArrayDeque<Character> stack = new ArrayDeque<>();
+            boolean balance = true;
             for (int i = 0; i < arr.length; i++) {
-                if (arr[i] == open[0] || arr[i] == open[1])
-                    bracket.add(arr[i]);
-
-                if (arr[i] == close[0]) {
-                    if (!bracket.isEmpty() && bracket.peek() == open[0])
-                        bracket.pop();
-                    else 
-                        isBalanced = false;
-                }
-                if (arr[i] == close[1]) {
-                    if (!bracket.isEmpty() && bracket.peek() == open[1])
-                        bracket.pop();
-                    else
-                        isBalanced = false;
+                if (arr[i] == '(' || arr[i] == '[') {
+                    stack.push(arr[i]);
+                } else if (arr[i] == ')') {
+                    if (stack.isEmpty() || stack.peek() != '(') {
+                        balance = false;
+                        break;
+                    } else {
+                        stack.pop();
+                    }
+                } else if (arr[i] == ']') {
+                    if (stack.isEmpty() || stack.peek() != '[') {
+                        balance = false;
+                        break;
+                    } else {
+                        stack.pop();
+                    }
                 }
             }
-            if (isBalanced && bracket.isEmpty()) 
-                sb.append("yes").append("\n");
-            else
-                sb.append("no").append("\n");
+            
+            sb.append(balance && stack.isEmpty() ? "yes" : "no").append("\n");
         }
+
         System.out.print(sb);
     }
 }
