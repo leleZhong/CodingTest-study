685e38357e7ae0dcc966f0080a190eabde5db968
Zhong <105035178+leleZhong@users.noreply.github.com>
2025-07-28 21:30:10 +0900

    [Silver IV] Title: solved.ac, Time: 268 ms, Memory: 35160 KB -BaekjoonHub

--- 백준/Silver/18110. solved．ac/solved．ac.java
+++ 백준/Silver/18110. solved．ac/solved．ac.java
@@ -7,23 +7,24 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        int[] arr = new int[n];
-        for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(br.readLine());
-        }
-
+        
         if (n == 0) {
             System.out.print(0);
             return;
         }
 
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
         Arrays.sort(arr);
-        int except = (int) Math.round(n * 0.15);
-        float sum = 0;
-        for (int i = except; i < n - except; i++) {
+
+        int border = (int) Math.round(n * 0.15);
+        int sum = 0;
+        for (int i = border; i < n - border; i++) {
             sum += arr[i];
         }
-        int ans = (int) Math.round(sum / (n - except * 2));
-        System.out.print(ans);
+
+        System.out.print(Math.round((double)sum / (n - border * 2)));
     }
 }
