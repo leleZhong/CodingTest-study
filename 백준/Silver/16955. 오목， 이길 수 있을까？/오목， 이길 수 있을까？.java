34dd3980341ed18b724b2eeaa6008c6501473367
Zhong <105035178+leleZhong@users.noreply.github.com>
2025-08-02 18:50:39 +0900

    [Silver IV] Title: 오목, 이길 수 있을까?, Time: 64 ms, Memory: 11492 KB -BaekjoonHub

--- 백준/Silver/16955. 오목， 이길 수 있을까？/오목， 이길 수 있을까？.java
+++ 백준/Silver/16955. 오목， 이길 수 있을까？/오목， 이길 수 있을까？.java
@@ -3,67 +3,104 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
+    static char[][] arr = new char[10][10];
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        char[][] board = new char[10][10];
         for (int i = 0; i < 10; i++) {
-            board[i] = br.readLine().toCharArray();
+            arr[i] = br.readLine().toCharArray();
         }
 
-        boolean possible = false;
         for (int i = 0; i < 10; i++) {
             for (int j = 0; j < 10; j++) {
-                if (board[i][j] == '.') {
-                    if (find(board, i, j)) {
-                        possible = true;
-                        break;
+                if (arr[i][j] == '.') {
+                    arr[i][j] = 'X';
+                    if (row(i, j) || col(i, j) || cross(i, j)) {
+                        System.out.print(1);
+                        return;
                     }
+                    arr[i][j] = '.';
                 }
             }
-            if (possible)
-                break;
         }
-        System.out.print(possible ? 1 : 0);
+
+        System.out.print(0);
     }
 
-    static boolean find(char[][] arr, int x, int y) {
-        int[] dx = { 0, 1, 1, 1 };
-        int[] dy = { 1, 0, 1, -1 };
-        for (int d = 0; d < 4; d++) {
-            int cnt = 1;
-            for (int i = 1; i < 5; i++) {
-                int nx = x + dx[d] * i;
-                int ny = y + dy[d] * i;
-                if (!isValid(nx, ny))
-                    break;
-
-                if (arr[nx][ny] == 'X')
+    static boolean row(int x, int y) {
+        int cnt = 0;
+        for (int i = -4; i <= 4; i++) {
+            int ny = y + i;
+            if (isValid(x, ny)) {
+                if (arr[x][ny] == 'X') {
                     cnt++;
-                else
-                    break;
+                } else {
+                    cnt = 0;
+                }
             }
-            for (int i = 1; i < 5; i++) {
-                int nx = x - dx[d] * i;
-                int ny = y - dy[d] * i;
-                if (!isValid(nx, ny))
-                    break;
-
-                if (arr[nx][ny] == 'X')
-                    cnt++;
-                else
-                    break;
-            }
-            if (cnt >= 5)
+            if (cnt >= 5) {
                 return true;
+            }
+        }
+        return false;
+    }
 
+    static boolean col(int x, int y) {
+        int cnt = 0;
+        for (int i = -4; i <= 4; i++) {
+            int nx = x + i;
+            if (isValid(nx, y)) {
+                if (arr[nx][y] == 'X') {
+                    cnt++;
+                } else {
+                    cnt = 0;
+                }
+            }
+            if (cnt >= 5) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    static boolean cross(int x, int y) {
+        int cnt1 = 0, cnt2 = 0;
+        for (int i = -4; i <= 4; i++) {
+            int nx = x + i;
+            int ny = y - i;
+            if (isValid(nx, ny)) {
+                if (arr[nx][ny] == 'X') {
+                    cnt1++;
+                } else {
+                    cnt1 = 0;
+                }
+            }
+            if (cnt1 >= 5) {
+                return true;
+            }
         }
 
+        for (int i = -4; i <= 4; i++) {
+            int nx = x + i;
+            int ny = y + i;
+            if (isValid(nx, ny)) {
+                if (arr[nx][ny] == 'X') {
+                    cnt2++;
+                } else {
+                    cnt2 = 0;
+                }
+            }
+            if (cnt2 >= 5) {
+                return true;
+            }
+        }
         return false;
     }
 
     static boolean isValid(int x, int y) {
-        if (x < 0 || x > 9 || y < 0 || y > 9)
-            return false;
-        return true;
+        if (x >= 0 && x < 10 && y >= 0 && y < 10) {
+            return true;
+        }
+        return false;
     }
 }
