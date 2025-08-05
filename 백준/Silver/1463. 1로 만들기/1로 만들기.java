efac73dc00c0977e543536abef442942595194fc
Zhong <105035178+leleZhong@users.noreply.github.com>
2025-08-05 19:56:31 +0900

    [Silver III] Title: 1로 만들기, Time: 64 ms, Memory: 12492 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -2,21 +2,21 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
-import java.util.Queue;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        boolean[] visited = new boolean[N + 1];
-        Queue<Integer> q = new ArrayDeque<>();
+
+        ArrayDeque<Integer> q = new ArrayDeque<>();
         q.add(N);
+        boolean[] visited = new boolean[N + 1];
         visited[N] = true;
-        int depth = 0; // 연산 횟수
+        int depth = 0;
 
         while (!q.isEmpty()) {
             int size = q.size();
-            for (int i = 0; i < size; i++) {
+            while (size-- > 0) {
                 int cur = q.poll();
                 if (cur == 1) {
                     System.out.print(depth);
@@ -38,6 +38,5 @@ public class Main {
             }
             depth++;
         }
-        System.out.print(depth);
     }
 }
