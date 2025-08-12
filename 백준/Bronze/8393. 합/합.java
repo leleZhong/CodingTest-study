ef1ae599efdce69f79e5205806588fe097b3f08a
Zhong <105035178+leleZhong@users.noreply.github.com>
2025-08-12 21:39:37 +0900

    [Bronze V] Title: 합, Time: 68 ms, Memory: 11492 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int sum = 0;
+        for (int i = Integer.parseInt(br.readLine()); i >= 1; i--) {
+            sum += i;
+        }
+
+        System.out.print(sum);
+    }
+}
