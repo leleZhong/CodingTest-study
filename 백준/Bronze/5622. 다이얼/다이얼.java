998f06c389d1f644f9c0c9b99e4de281e5f92621
Zhong <105035178+leleZhong@users.noreply.github.com>
2025-08-14 21:55:50 +0900

    [Bronze II] Title: 다이얼, Time: 68 ms, Memory: 11480 KB -BaekjoonHub

---
+++ 백준/Bronze/5622. 다이얼/다이얼.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        char[] arr = br.readLine().toCharArray();
+
+        int[] alphabet = { 2, 2, 2, 3, 3, 3, 4, 4, 4, 5, 5, 5, 6, 6, 6, 7, 7, 7, 7, 8, 8, 8, 9, 9, 9, 9 };
+
+        int ans = 0;
+        for (int i = 0; i < arr.length; i++) {
+            ans += alphabet[arr[i] - 'A'] + 1;
+        }
+
+        System.out.print(ans);
+    }
+}
