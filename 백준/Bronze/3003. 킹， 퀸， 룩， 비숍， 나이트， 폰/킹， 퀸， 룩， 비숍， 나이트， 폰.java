02146025cbde9f5002892381cae58d090fbdff7b
Zhong <105035178+leleZhong@users.noreply.github.com>
2025-08-14 22:20:45 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 68 ms, Memory: 11524 KB -BaekjoonHub

---
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int[] arr = {1, 1, 2, 2, 2, 8};
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < 6; i++) {
+            sb.append(arr[i] - Integer.parseInt(st.nextToken())).append(" ");
+        }
+
+        System.out.print(sb);
+    }
+}
