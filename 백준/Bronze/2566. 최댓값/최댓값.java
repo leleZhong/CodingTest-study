9182c7713a3f2bcae5d35bd00491171b61b9bab0
Zhong <105035178+leleZhong@users.noreply.github.com>
2025-08-21 11:00:44 +0900

    [Bronze III] Title: 최댓값, Time: 64 ms, Memory: 11604 KB -BaekjoonHub

--- 백준/Bronze/2566. 최댓값/최댓값.java
+++ 백준/Bronze/2566. 최댓값/최댓값.java
@@ -7,22 +7,20 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int max = 0;
-        int r = 0, c = 0;
-
-        for (int i = 0; i < 9; i++) {
+        int x = 0;
+        int y = 0;
+        for (int i = 1; i <= 9; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < 9; j++) {
-                int tmp = Integer.parseInt(st.nextToken());
-                if (max < tmp) {
-                    max = tmp;
-                    r = i;
-                    c = j;
+            for (int j = 1; j <= 9; j++) {
+                int num = Integer.parseInt(st.nextToken());
+                if (max <= num) {
+                    max = Math.max(max, num);
+                    x = i;
+                    y = j;
                 }
             }
         }
 
-        StringBuilder sb = new StringBuilder();
-        sb.append(max).append("\n").append(r+1).append(" ").append(c+1);
-        System.out.print(sb);
+        System.out.print(max + "\n" + x + " " + y);
     }
 }
