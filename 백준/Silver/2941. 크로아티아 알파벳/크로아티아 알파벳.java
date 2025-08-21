65315ad1a35299ca1f47a6dc393542a23728bfee
Zhong <105035178+leleZhong@users.noreply.github.com>
2025-08-21 10:18:59 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 64 ms, Memory: 11680 KB -BaekjoonHub

---
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+
+        String[] arr = {"c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="};
+        for (String alphabet : arr) {
+            s = s.replace(alphabet, "A");
+        }
+
+        System.out.print(s.length());
+    }
+}
