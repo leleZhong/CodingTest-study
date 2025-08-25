44b2af632d8292f327176ea5fa2d9839d451c27b
Zhong <105035178+leleZhong@users.noreply.github.com>
2025-08-25 23:58:58 +0900

    [Silver II] Title: 에디터, Time: 652 ms, Memory: 120152 KB -BaekjoonHub

--- 백준/Silver/1406. 에디터/에디터.java
+++ 백준/Silver/1406. 에디터/에디터.java
@@ -1,55 +1,54 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.LinkedList;
-import java.util.List;
 import java.util.ListIterator;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String input = br.readLine();
-        List<Character> list = new LinkedList<>();
-        for (int i = 0; i < input.length(); i++) {
-            list.add(input.charAt(i));
+        char[] input = br.readLine().toCharArray();
+        LinkedList<Character> list = new LinkedList<>();
+        for (int i = 0; i < input.length; i++) {
+            list.add(input[i]);
         }
+
         ListIterator<Character> cursor = list.listIterator();
         while (cursor.hasNext()) {
             cursor.next();
         }
 
         int M = Integer.parseInt(br.readLine());
-        for (int i = 0; i < M; i++) {
-            String command = br.readLine();
-            char c = command.charAt(0);
-            switch (c) {
-                case 'L':
-                    if (cursor.hasPrevious())
-                        cursor.previous();
-                    break;
-                case 'D':
-                    if (cursor.hasNext())
-                        cursor.next();
-                    break;
-                case 'B':
+        while (M-- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            switch (st.nextToken()) {
+                case "L":
                     if (cursor.hasPrevious()) {
                         cursor.previous();
-                        cursor.remove();    // remove()는 next()나 previous()로 반환된 최근 요소를 제거
                     }
                     break;
-                case 'P':
-                    char add = command.charAt(2);
-                    cursor.add(add);
+                case "D":
+                    if (cursor.hasNext()) {
+                        cursor.next();
+                    }
+                    break;
+                case "B":
+                    if (cursor.hasPrevious()) {
+                        cursor.previous();
+                        cursor.remove();
+                    }
+                    break;
+                case "P":
+                    cursor.add(st.nextToken().charAt(0));
+                    break;
             }
         }
 
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
         for (char c : list) {
-            bw.write(c);
+            sb.append(c);
         }
-        bw.flush();
-        bw.close();
+
+        System.out.print(sb);
     }
 }
