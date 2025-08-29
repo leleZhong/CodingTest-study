466724bf9312393cf363c78e9d1a7d4b874f5da8
Zhong <105035178+leleZhong@users.noreply.github.com>
2025-08-29 21:34:30 +0900

    [Bronze IV] Title: 헛간 청약, Time: 64 ms, Memory: 11504 KB -BaekjoonHub

---
+++ 백준/Bronze/19698. 헛간 청약/헛간 청약.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int W = Integer.parseInt(st.nextToken());
+        int H = Integer.parseInt(st.nextToken());
+        int L = Integer.parseInt(st.nextToken());
+        int cnt = (W / L) * (H / L);
+        System.out.print(cnt < N ? cnt : N);
+    }
+}
