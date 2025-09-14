a3d98ce5753a92ccbdb3387027719e6ff0a2655a
Zhong <105035178+leleZhong@users.noreply.github.com>
2025-09-14 20:27:01 +0900

    [Silver V] Title: 소트인사이드, Time: 68 ms, Memory: 11540 KB -BaekjoonHub

---
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        char[] arr = br.readLine().toCharArray();
+        Arrays.sort(arr);
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = arr.length - 1; i >= 0; i--) {
+            sb.append(arr[i]);
+        }
+
+        System.out.print(sb);
+    }
+}
