71ab3f5df22d6f8af767f6a5888add6713675d2e
Zhong <105035178+leleZhong@users.noreply.github.com>
2025-09-17 00:38:37 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 400 ms, Memory: 47508 KB -BaekjoonHub

--- 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.java
+++ 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.java
@@ -1,16 +1,15 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.Collections;
-import java.util.HashSet;
 import java.util.StringTokenizer;
+import java.util.TreeSet;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
 
-        HashSet<String> set = new HashSet<>();
+        TreeSet<String> set = new TreeSet<>(Collections.reverseOrder());
         while (N --> 0) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             String name = st.nextToken();
@@ -21,11 +20,8 @@ public class Main {
             }
         }
 
-        String[] arr = set.toArray(new String[0]);
-        Arrays.sort(arr, Collections.reverseOrder());
-
         StringBuilder sb = new StringBuilder();
-        for (String s : arr) {
+        for (String s : set) {
             sb.append(s).append("\n");
         }
 
