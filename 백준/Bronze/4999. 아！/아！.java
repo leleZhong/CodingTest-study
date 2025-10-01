68d4761689f5fb8d561d8617d659c27750e49bdb
Zhong <105035178+leleZhong@users.noreply.github.com>
2025-10-01 23:19:52 +0900

    [Bronze V] Title: 아!, Time: 68 ms, Memory: 11496 KB -BaekjoonHub

---
+++ 백준/Bronze/4999. 아！/아！.java
@@ -0,0 +1,10 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(br.readLine().length() >= br.readLine().length() ? "go" : "no");
+    }
+}
