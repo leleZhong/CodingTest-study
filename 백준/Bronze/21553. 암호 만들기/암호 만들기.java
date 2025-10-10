18944c554dd16f8edc85b2f868efa435ef275d0a
Zhong <105035178+leleZhong@users.noreply.github.com>
2025-10-10 20:53:05 +0900

    [Bronze III] Title: 암호 만들기, Time: 68 ms, Memory: 11500 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/21553. 암호 만들기/암호 만들기.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String A = br.readLine();
+        String P = br.readLine();
+
+        System.out.println(P);
+    }
+}
