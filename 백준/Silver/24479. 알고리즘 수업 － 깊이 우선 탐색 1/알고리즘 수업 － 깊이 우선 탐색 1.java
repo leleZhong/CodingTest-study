cde831bc2d3760606bc64c6bfbf765028917f5b3
Zhong <105035178+leleZhong@users.noreply.github.com>
2025-10-19 00:05:58 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 1, Time: 836 ms, Memory: 94244 KB -BaekjoonHub

---
+++ 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.java
@@ -0,0 +1,58 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+class Main {
+    static ArrayList<Integer>[] graph;
+    static int[] ans;
+    static int cnt = 1;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int R = Integer.parseInt(st.nextToken());
+
+        graph = new ArrayList[N + 1];
+        for (int i = 1; i <= N; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        while (M --> 0) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            graph[x].add(y);
+            graph[y].add(x);
+        }
+
+        for (int i = 1; i <= N; i++) {
+            Collections.sort(graph[i]);
+        }
+
+        ans = new int[N + 1];
+        recur(R);
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= N; i++) {
+            sb.append(ans[i]).append("\n");
+        }
+        System.out.println(sb);
+    }
+
+    static void recur(int r) {
+        ans[r] = cnt++;
+
+        for (int next : graph[r]) {
+            if (ans[next] > 0) {
+                continue;
+            }
+
+            recur(next);
+        }
+    }
+}
