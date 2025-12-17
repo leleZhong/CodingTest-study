34f9a2d80ffa9011244b54302eed02cea124bc23
Zhong <105035178+leleZhong@users.noreply.github.com>
2025-12-17 21:31:39 +0900

    [Bronze V] Title: 라면 공식, Time: 68 ms, Memory: 11664 KB -BaekjoonHub

---
+++ 백준/Bronze/30007. 라면 공식/라면 공식.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+        while (N --> 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+            int X = Integer.parseInt(st.nextToken());
+            sb.append(A * (X - 1) + B).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
