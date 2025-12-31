fc3739541dc4b5fb0f404fd9cd569a733aac9401
Zhong <105035178+leleZhong@users.noreply.github.com>
2025-12-31 23:09:31 +0900

    [Bronze V] Title: 특별한 학교 이름, Time: 60 ms, Memory: 11428 KB -BaekjoonHub

---
+++ 백준/Bronze/27889. 특별한 학교 이름/특별한 학교 이름.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        switch (br.readLine()) {
+            case "NLCS":
+                System.out.println("North London Collegiate School");
+                break;
+            case "BHA":
+                System.out.println("Branksome Hall Asia");
+                break;
+            case "KIS":
+                System.out.println("Korea International School");
+                break;
+            case "SJA":
+                System.out.println("St. Johnsbury Academy");
+                break;
+        }
+    }
+}
