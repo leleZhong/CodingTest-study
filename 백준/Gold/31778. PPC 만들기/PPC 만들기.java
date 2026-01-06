7c66f8c664c101b8776ce772fd71b795e4c88b29
Zhong <105035178+leleZhong@users.noreply.github.com>
2026-01-06 13:34:54 +0900

    [Gold III] Title: PPC 만들기, Time: 124 ms, Memory: 17252 KB -BaekjoonHub

---
+++ 백준/Gold/31778. PPC 만들기/PPC 만들기.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        char[] arr = br.readLine().toCharArray();
+        
+        // 앞쪽 C와 뒤쪽 P 교환 (최대 K번)
+        int left = 0, right = N - 1;
+        int swaps = 0;
+        while (left < right && swaps < K) {
+            while (left < right && arr[left] != 'C') left++;
+            while (left < right && arr[right] != 'P') right--;
+            if (left < right) {
+                arr[left] = 'P';
+                arr[right] = 'C';
+                left++;
+                right--;
+                swaps++;
+            }
+        }
+
+        int[] prefix = new int[N + 1];
+        for (int i = 1; i <= N; i++) {
+            prefix[i] = prefix[i - 1] + (arr[i - 1] == 'P' ? 1 : 0);
+        }
+
+        long ans = 0;
+        for (int i = 1; i <= N; i++) {
+            if (arr[i - 1] == 'C') {
+                ans += (long) prefix[i] * (prefix[i] - 1) / 2;
+            }
+        }
+
+        System.out.println(ans);
+    }
+}
