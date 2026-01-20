1c9071e474ecfceb4fbed18b97b73547fdfeaf71
Zhong <105035178+leleZhong@users.noreply.github.com>
2026-01-20 22:01:41 +0900

    [Silver II] Title: 황소 다마고치, Time: 72 ms, Memory: 12464 KB -BaekjoonHub

---
+++ 백준/Silver/23323. 황소 다마고치/황소 다마고치.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+        while (T-- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            long n = Long.parseLong(st.nextToken());
+            long m = Long.parseLong(st.nextToken());
+
+            int cnt = 0;
+            while (n > 0) {
+                cnt++;
+                n /= 2;
+            }
+            sb.append(cnt + m).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
