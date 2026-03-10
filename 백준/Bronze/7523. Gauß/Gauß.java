7bd3e040c73db75784f1e1eb769aa9fefde10648
Zhong <105035178+leleZhong@users.noreply.github.com>
2026-03-10 23:48:33 +0900

    [Bronze II] Title: Gauß, Time: 80 ms, Memory: 12148 KB -BaekjoonHub

---
+++ 백준/Bronze/7523. Gauß/Gauß.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+        for (int tc = 1; tc <= T; tc++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            long a = Long.parseLong(st.nextToken());
+            long b = Long.parseLong(st.nextToken());
+
+            long ans = (a + b) * (b - a + 1) / 2;
+
+            sb.append("Scenario #").append(tc).append(":").append("\n").append(ans).append("\n\n");
+        }
+        System.out.println(sb);
+    }
+}
